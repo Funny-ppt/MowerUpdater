7502932c01ed7652d3257b76b7266ef949bd8e8a
funny-ppt <1763341376@qq.com>
2023-10-05 14:47:57 +0800

    修复pack工具的问题

--- pack/pack.bat
+++ pack/pack.bat
@@ -1,9 +1,12 @@
 @echo off
-copy /Y ../bin/Release/* bin
+copy /Y "..\bin\Release\*" "bin\"
+
 del archive.7z
-cd bin
-../7zr.exe a ../archive.7z *
-cd ..
 del MowerUpdater.exe
+
+cd bin
+"../7zr.exe" a ../archive.7z *
+cd ..
+
 copy /Y /B 7zSD.sfx + config.txt + archive.7z MowerUpdater.exe
 pause >nul
